635707a71559152d2886b189c877f9e25e7c79f6
kurakura1122 <k.kosuke12345@icloud.com>
2026-04-21 01:35:35 +0900

    add src/components/ui/toast.jsx

---
+++ src/components/ui/toast.jsx
@@ -0,0 +1,6 @@
+import * as React from "react";
+import { cva } from "class-variance-authority";
+import { X } from "lucide-react";
+import { cn } from "@/lib/utils";
+
+export { Toast, ToastAction, ToastClose, ToastDescription, ToastProvider, ToastTitle, ToastViewport };
