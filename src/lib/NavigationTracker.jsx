4bc5e14e7197e3b7badc3a1512c1fc25142068b6
kurakura1122 <k.kosuke12345@icloud.com>
2026-04-21 01:35:40 +0900

    add src/lib/NavigationTracker.jsx

---
+++ src/lib/NavigationTracker.jsx
@@ -0,0 +1,7 @@
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import { useAuth } from './AuthContext';
+
+export default function NavigationTracker() {
+  return null;
+}
